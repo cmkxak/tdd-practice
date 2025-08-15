9ad33363c50f50de0c534872c4befc344ed2c2a2
cmkxak <gcm466@naver.com>
2025-08-15 13:27:15 +0900

    feat : 포인트 충전 테스트 코드 작성

--- src/test/java/io/hhplus/tdd/point/application/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/application/PointServiceTest.java
@@ -1,4 +1,48 @@
-import static org.junit.jupiter.api.Assertions.*;
+package io.hhplus.tdd.point.application;
+
+import io.hhplus.tdd.HHPlusAppExcetion;
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.PointHistory;
+import io.hhplus.tdd.point.TransactionType;
+import io.hhplus.tdd.point.UserPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class PointServiceTest {
-  
+
+    private PointService pointService;
+    private PointHistoryTable pointHistoryTable;
+    private UserPointTable userPointTable;
+
+    @BeforeEach
+    void setUp() {
+        pointHistoryTable = new PointHistoryTable();
+        userPointTable = new UserPointTable();
+        pointService = new PointService(pointHistoryTable, userPointTable);
+
+        UserPoint userPoint = new UserPoint(1, 10000, System.currentTimeMillis());
+        userPointTable.insertOrUpdate(userPoint.id(), userPoint.point());
+        pointHistoryTable.insert(userPoint.id(), userPoint.point(), TransactionType.CHARGE, userPoint.updateMillis());
+    }
+
+    @Test
+    void charge() {
+        //given
+        long id = 1;
+        long amount = 15000;
+
+        //when
+        UserPoint updatedUserPoint = pointService.charge(id, amount);
+
+        //then
+        assertThat(updatedUserPoint.point()).isEqualTo(25000);
+    }
+
 }
