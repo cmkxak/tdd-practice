b88c5668776c0785462a444fd0cb86567aae70fc
cmkxak <gcm466@naver.com>
2025-08-15 13:25:04 +0900

    feat : 에러 코드 추가

--- src/main/java/io/hhplus/tdd/point/ErrorCode.java
+++ src/main/java/io/hhplus/tdd/point/ErrorCode.java
@@ -1,2 +1,16 @@
-package io.hhplus.tdd.point;public enum ErrorCode {
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.ErrorResponse;
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    INVALID_BALANCE(new ErrorResponse("ERR-100", "잔액이 부족합니다."));
+
+    private final ErrorResponse errorResponse;
+
+    ErrorCode(ErrorResponse errorResponse) {
+        this.errorResponse = errorResponse;
+    }
 }
